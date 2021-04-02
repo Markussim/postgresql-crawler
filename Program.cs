b170d3bb9dca292594fbccc9131ed6010cb207b7
Markussim <markus.simonsen@gmail.com>
2021-04-02 15:23:58 +0200

    Added vscode things

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
-namespace postgresql_crawler
+namespace EFGetStarted
 {
-    class Program
+    public class BloggingContext : DbContext
     {
-        static void Main(string[] args)
+        public DbSet<Persons4> people { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseNpgsql("Host=localhost;Database=my_ver_nice_db;Username=postgres;Password=1");
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BloggingContext())
+            {
+                // Note: This sample requires the database to be created before running.
+
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Persons4 { personID = 12 });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.people;
+
+                Console.WriteLine(blog);
+                db.SaveChanges();
+            }
         }
+
     }
+
+    public class Persons4
+    {
+        public int Id { get; set; }
+
+        public int personID { get; set; }
+    }
+
+
+
+
 }
