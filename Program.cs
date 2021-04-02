1f398c47edc672942761847a076f176a0385fbc7
Markussim <markus.simonsen@gmail.com>
2021-04-02 18:35:27 +0200

    Moved to main2

--- Program.cs
+++ Program.cs
@@ -20,38 +20,36 @@ namespace EFGetStarted
         {
             using (var db = new BloggingContext())
             {
-                // Note: This sample requires the database to be created before running.
-
-                // Create
-                Console.WriteLine("Inserting a new blog");
-                db.Add(new people { customer_id = 12 });
-                db.SaveChanges();
-
-                // Read
-                Console.WriteLine("Querying for a blog");
-                var blog = db.customers_orders2;
-                db.SaveChanges();
-
-                //Console.WriteLine(blog.Find(1).customer_id);
-
-
-                GetList(blog);
-
-
-                db.SaveChanges();
-
-                //Thread.Sleep(1000);
+                Main2(db).Wait();
             }
         }
 
-        private static async Task GetList(DbSet<people> input)
+        private static async Task Main2(BloggingContext db)
         {
+            // Note: This sample requires the database to be created before running.
+
+            // Create
+            Console.WriteLine("Inserting a new blog");
+            db.Add(new people { customer_id = 12 });
+            db.SaveChanges();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+            var blog = db.customers_orders2;
+            db.SaveChanges();
+
+            //Console.WriteLine(blog.Find(1).customer_id);
+
             CancellationToken token = default;
             Console.WriteLine("Test");
-            foreach (var item in await input.ToListAsync(token))
+            foreach (var item in await blog.ToListAsync(token))
             {
                 Console.WriteLine(item.customer_id);
             }
+
+            db.SaveChanges();
+
+            //Thread.Sleep(1000);
         }
 
     }
