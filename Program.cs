5251e4f6afce67ca2b76cc88db5dc195aa9a4e71
Markussim <markus.simonsen@gmail.com>
2021-04-02 18:18:11 +0200

    Get full table

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+
 
 namespace EFGetStarted
 {
     public class BloggingContext : DbContext
     {
         public DbSet<people> customers_orders2 { get; set; }
+        static ManualResetEvent resetEvent = new ManualResetEvent(false);
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseNpgsql("Host=localhost;Database=my_ver_nice_db;Username=postgres;Password=1");
@@ -27,10 +32,25 @@ namespace EFGetStarted
                 var blog = db.customers_orders2;
                 db.SaveChanges();
 
+                //Console.WriteLine(blog.Find(1).customer_id);
 
-                Console.WriteLine(blog.Find(1).customer_id);
+
+                GetList(blog);
+
 
                 db.SaveChanges();
+
+                //Thread.Sleep(1000);
+            }
+        }
+
+        private static async Task GetList(DbSet<people> input)
+        {
+            CancellationToken token = default;
+            Console.WriteLine("Test");
+            foreach (var item in await input.ToListAsync(token))
+            {
+                Console.WriteLine(item.customer_id);
             }
         }
 
